23574f9fb96c2d9c3794cb151de2c0b904483a1c
Vishal Salgond <vnsalgond@gmail.com>
2020-05-28 02:09:57 +0530

    added ckeditor

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -131,3 +132,11 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+
+CKEDITOR_CONFIGS = {
+    'default': {
+    'height': 300,
+    'width': 680,
+   }, 
+}
