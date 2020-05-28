3c21b150f754038eb4ed952901f45ac004692af1
Vishal Salgond <vnsalgond@gmail.com>
2020-05-29 00:59:47 +0530

    admin updated

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Question, Answer
 
 admin.site.register(Profile)
+admin.site.register(Question)
+admin.site.register(Answer)
